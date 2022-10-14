5dfba4ff4ae108016c3e873fa6ddf958310ad4d5
MikeyGloriani <MikeyGloriani@users.noreply.github.com>
2022-10-14 18:24:23 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@
 // Created on: Oct 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
  * This function calculates area and perimeter of rectangle.
  */
-function calculate () {
+function calculate() {
   // input
-  const base = parseInt(document.getElementById('base-of-triangle').value)
-  const height = parseInt(document.getElementById('height-of-triangle').value)
+  const base = parseInt(document.getElementById("base-of-triangle").value)
+  const height = parseInt(document.getElementById("height-of-triangle").value)
 
   // process
-  const area = (base * height)/2
+  const area = (base * height) / 2
 
   // output
-  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
 }
